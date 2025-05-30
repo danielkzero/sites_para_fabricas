f419e289478c48b2477054d261bc5b3125fa8e4b
Daniel S. Ramos <danikzero@hotmail.com>
2025-05-30 16:51:22 -0300

    :sparkles: feat: Plugin para o auto-animate

---
+++ plugins/auto-animate.client.ts
@@ -0,0 +1,10 @@
+import { defineNuxtPlugin } from "#app";
+import autoAnimate from "@formkit/auto-animate";
+
+export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.vueApp.directive("auto-animate", {
+        mounted(el) {
+            autoAnimate(el);
+        },
+    });
+});
