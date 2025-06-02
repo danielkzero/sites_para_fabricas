5b5a8d38a65f03ba32fb6a4d4493c9e562dfe3eb
Daniel S. Ramos <danikzero@hotmail.com>
2025-06-02 08:28:23 -0300

    :sparkles: feat: novo componente

---
+++ components/card/destaques.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="py-16">
+            <div class="max-w-6xl mx-auto px-4">
+                <h2 class="text-2xl font-bold text-center mb-12">Por que escolher a GlobalPlastic?</h2>
+                <div class="grid md:grid-cols-3 gap-10">
+                    <div class="text-center">
+                        <i class="bx bx-bulb text-rose-800 text-5xl mb-4"></i>
+                        <h4 class="font-semibold text-lg">Inovação Constante</h4>
+                        <p class="text-sm text-gray-600">Estamos sempre criando novas soluções para facilitar sua vida.
+                        </p>
+                    </div>
+                    <div class="text-center">
+                        <i class="bx bx-shield-quarter text-rose-800 text-5xl mb-4"></i>
+                        <h4 class="font-semibold text-lg">Qualidade Garantida</h4>
+                        <p class="text-sm text-gray-600">Produtos duráveis, práticos e com alto padrão de produção.</p>
+                    </div>
+                    <div class="text-center">
+                        <i class="bx bx-heart text-rose-800 text-5xl mb-4"></i>
+                        <h4 class="font-semibold text-lg">Compromisso com Você</h4>
+                        <p class="text-sm text-gray-600">Atendimento focado em garantir sua satisfação total.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </template>
