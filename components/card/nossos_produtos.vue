b201c777d68df4fac5de698af2877f259cb6e3eb
Daniel S. Ramos <danikzero@hotmail.com>
2025-06-02 17:49:25 -0300

    :recycle: refactor: remoção de chaves de array não mais utilizada

--- components/card/nossos_produtos.vue
+++ components/card/nossos_produtos.vue
@@ -1,15 +1,17 @@
 <template>
     <div :class="classSection">
-        <span class="text-3xl font-bold text-center mb-8">{{ titulo }}</span>
-        <div :class="classGrid">
-            <div v-for="item in produtos" :key="item.nome" :class="classCard">
-                <img :src="item.img" :alt="item.nome" :class="classImagem" />
-                <div :class="classTitulo">{{ item.nome }}</div>                    
+        <div :class="classContainer">
+            <span class="text-3xl font-bold text-center mb-8 block" v-if="titulo">{{ titulo }}</span>
+                <div :class="classGrid">
+                <div v-for="item in produtos" :key="item.nome" :class="classCard">
+                    <img :src="item.img" :alt="item.nome" :class="classImagem" />
+                    <div :class="classTitulo">{{ item.nome }}</div>                    
                     <button :class="classButton">
                         {{ buttonText }}
                     </button>
+                </div>
             </div>
-        </div>
+        </div>        
     </div>
 </template>
 
@@ -22,12 +24,12 @@ defineProps<{
     classTitulo?: string;
     classDescricao?: string;
     classButton?: string;
+    classContainer?: string;
     buttonText?: string;
     titulo: string;
     produtos: Array<{
         nome: string;
         img: string;
-        linha: string;
     }>;
 }>();
 </script>
