5a9d95c929052e1476b823de24553ca5644660b0
Daniel S. Ramos <danikzero@hotmail.com>
2025-06-09 16:00:20 -0300

    :sparkles: feat: Menu da área administrativa

---
+++ components/admin-sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+  <aside class="w-16 bg-white dark:bg-gray-800 shadow-md flex flex-col items-center py-4 space-y-6">
+    <NuxtLink to="/admin" class="hover:text-blue-500" title="Dashboard">
+      <i class="bx bxs-dashboard text-2xl"></i>
+    </NuxtLink>
+    <NuxtLink to="/admin/pages" class="hover:text-blue-500" title="Páginas">
+      <i class="bx bxs-file text-2xl"></i>
+    </NuxtLink>
+    <NuxtLink to="/admin/config" class="hover:text-blue-500" title="Configurações">
+      <i class="bx bxs-cog text-2xl"></i>
+    </NuxtLink>
+  </aside>
+</template>
+
+<script setup>
+</script>
