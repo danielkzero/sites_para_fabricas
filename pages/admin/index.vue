05d41335576b25845adfe0fd6fb264d7c88d7feb
Daniel S. Ramos <danikzero@hotmail.com>
2025-06-09 16:06:44 -0300

    :sparkles: feat: Página principal da área administrativa de teste

---
+++ pages/admin/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p class="text-gray-600 dark:text-gray-300">Bem-vindo ao Modest CMS.</p>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({ layout: 'admin' })
+</script>
