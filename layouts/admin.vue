66140d08843a5622b213f94cfacb4fb0dc90c647
Daniel S. Ramos <danikzero@hotmail.com>
2025-06-09 16:01:03 -0300

    :sparkles: feat: Layout da área administrativa do modest

---
+++ layouts/admin.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-white">
+    <AdminSidebar />
+
+    <div class="flex-1 flex flex-col overflow-hidden">
+      <!-- Content -->
+      <small class="p-3 pb-0 text-gray-400 text-[0.6rem]">Modest CMS 1.0.0.0</small>
+      <main class="flex-1 overflow-y-auto p-3">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
