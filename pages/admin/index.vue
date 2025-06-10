c2b17466c430f832ec0c3ce0342f306a2d8d6e41
Daniel S. Ramos <danikzero@hotmail.com>
2025-06-10 16:15:32 -0300

    :wastebasket: remove: Remoção do index para a parte admin, agora irá como dashboard como principal

--- pages/admin/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <p class="text-gray-600 dark:text-gray-300">Bem-vindo ao Modest CMS.</p>
-  </div>
-</template>
-
-<script setup>
-definePageMeta({ layout: 'admin' })
-</script>
